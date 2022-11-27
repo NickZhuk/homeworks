c39817663cb337a28066b47248afa553fe872f5a
NickZhuk <118761949+NickZhuk@users.noreply.github.com>
2022-11-27 16:20:29 +0300

    Fixed task three.py

--- homeworks3/task_three.py
+++ homeworks3/task_three.py
@@ -1,6 +1,6 @@
 from random import randint
 lst = [randint(-100, 100) for i in range(10)]
 print(lst)
-lst[2] = [randint(-100, 100) for i in range(1)]
+lst[2] = randint(-100, 100)
 del lst[6]
 print(lst)
