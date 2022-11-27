d94850137f6aba4a3b826d3c221126b4591485aa
NickZhuk <118761949+NickZhuk@users.noreply.github.com>
2022-11-27 16:19:47 +0300

    Fixed task two.py

--- homeworks3/task_two.py
+++ homeworks3/task_two.py
@@ -1,4 +1,2 @@
-line = input()
-first_word = line[:line.find(' ')]
-second_word = line[line.find(' ') + 1:]
-print(second_word + ' ' + first_word)
+line = ' '.join(input().split(' ')[::-1])
+print(line)
