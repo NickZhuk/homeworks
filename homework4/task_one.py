bc148b82b3e45dda394de0a19d0f9968dcec9f19
NickZhuk <118761949+NickZhuk@users.noreply.github.com>
2022-12-03 16:11:41 +0300

    Update task_one.py

--- homework4/task_one.py
+++ homework4/task_one.py
@@ -1,12 +1,11 @@
-from collections import defaultdict
-dict1 = {'a': 1, 'b': 2, 'c': 3}
-dict2 = {'c': 3, 'd': 4, 'e': 5}
-dict3 = defaultdict(list)
-for key in set(list(dict1.keys())+list(dict2.keys())):
-    if key in dict1:
-        dict3[key].append(dict1[key])
-        dict3[key].insert(0, None)
-    if key in dict2:
-        dict3[key].append(dict2[key])
-        dict3[key].append(None)
-print(dict3)
+a = {'a': 1, 'b': 2, 'c': 3}
+b = {'c': 3, 'd': 4, 'e': 5}
+ab = dict()
+a_keys = list(a.keys())
+b_keys = list(b.keys())
+a_set_keys = set(a_keys)
+b_set_keys = set(b_keys)
+common_keys = list(a_set_keys.union(b_set_keys))
+for key in common_keys:
+    ab[key] = [a.get(key), b.get(key)]
+print(ab)
