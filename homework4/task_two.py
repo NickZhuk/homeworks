b31bccc8c02038371a9aacbbc0b023d06b6ff856
NickZhuk <118761949+NickZhuk@users.noreply.github.com>
2022-12-03 16:14:20 +0300

    Update task_two.py

--- homework4/task_two.py
+++ homework4/task_two.py
@@ -1,9 +1,9 @@
-for i in range(1, 101, 1):
-    if i % 3 == 0:
-        print('Fuzz')
-    if i % 5 == 0:
-        print('Buzz')
+for i in range(1, 101):
     if i % 3 == 0 and i % 5 == 0:
         print('FuzzBuzz')
+    elif i % 3 == 0:
+        print('Fuzz')
+    elif i % 5 == 0:
+        print('Buzz')
     else:
         print(i)
